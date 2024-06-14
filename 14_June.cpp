f7b3101540606ab7164383bde1faae50a355b9d0
niketsuchak <93976932+niketsuchak@users.noreply.github.com>
2024-06-14 14:31:58 +0530

    Update 14_June.cpp

--- 14_June.cpp
+++ 14_June.cpp
@@ -1,19 +1,7 @@
+//Minimum increment to make array unique
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        int n = nums.size();
-        int count = 0;
-
-        sort(nums.begin(), nums.end());
-
-        for (int i = 1; i < n; i++) {
-            if (nums[i] <= nums[i-1]) {
-                int increment = nums[i-1] + 1 - nums[i];
-                nums[i] = nums[i-1] + 1;
-                count += increment;
-            }
-        }
-
-        return count;
+        
     }
 };
