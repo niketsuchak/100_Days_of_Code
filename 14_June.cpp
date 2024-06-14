ba769429a3dd76766b590c2524701ed4fc388a88
niketsuchak <93976932+niketsuchak@users.noreply.github.com>
2024-06-14 14:33:56 +0530

    Update 14_June.cpp

--- 14_June.cpp
+++ 14_June.cpp
@@ -5,8 +5,16 @@ public:
         int n = nums.size();
         int count = 0;
         sort(nums.begin(), nums.end());
-        
-        
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] <= nums[i-1]) {
+                int increment = nums[i-1] + 1 - nums[i];
+                nums[i] = nums[i-1] + 1;
+                count += increment;
+            }
+        }
+
+        return count;
         
     }
 };
